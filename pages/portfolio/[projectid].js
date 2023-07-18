305929a3961222ae543ab882768369a9cd49ce61
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-18 16:23:01 +0300

    Adding Dynamic Path&Routes

---
+++ pages/portfolio/[projectid].js
@@ -0,0 +1,7 @@
+function PortfolioProjectPage() {
+    return <div>
+        <h1>The Portfolio Project Page</h1>
+    </div>
+}
+
+export default PortfolioProjectPage; // This page serves at http://localhost:3000/portfolio/ANYTHING YOU WRITE EXCEPT OURT PLACEHOLDER FILES
