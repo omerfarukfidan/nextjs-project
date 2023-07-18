b1c0df7991825c3b9cda7cbb738d8dd0ca9fdb00
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-18 16:42:21 +0300

    Extracting Dynamic Path Segment Data (Dynamic Routes)

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,7 +1,17 @@
+import {useRouter} from 'next/router'
+
 function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);
+
+    // send a request to some backend server 
+    // to fetch the piece of data with an id of router.query.projectid
+
     return <div>
         <h1>The Portfolio Project Page</h1>
     </div>
 }
 
-export default PortfolioProjectPage; // This page serves at http://localhost:3000/portfolio/ANYTHING YOU WRITE EXCEPT OURT PLACEHOLDER FILES
+export default PortfolioProjectPage; // This page serves at http://localhost:3000/portfolio/ANYTHING YOU WRITE EXCEPT OUR PLACEHOLDER FILES
