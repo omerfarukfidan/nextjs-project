f7237ddf8d485e15186cae65e032de688d92b2e0
omerfarukfidan <fidanomerfaruk01@gmail.com>
2023-07-18 17:03:30 +0300

    Building Nested Dynamic Routes & Paths

---
+++ pages/clients/index.js
@@ -0,0 +1,10 @@
+
+function ClientsPage() {
+  return (
+    <div>
+      <h1>The Client Page</h1>
+    </div>
+  );
+}
+
+export default ClientsPage; // Which serves at http://localhost:3000/clients
